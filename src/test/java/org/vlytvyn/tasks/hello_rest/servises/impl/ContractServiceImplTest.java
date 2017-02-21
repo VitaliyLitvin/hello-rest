7a63b2b5c2315d73a856de3d7ff467e3b5401d0f
VitaliyLitvin <vitaliy.litvinn@gmail.com>
2017-02-21 17:45:11 +0200

    Fix tests.

--- src/test/java/org/vlytvyn/tasks/hello_rest/servises/impl/ContractServiceImplTest.java
+++ src/test/java/org/vlytvyn/tasks/hello_rest/servises/impl/ContractServiceImplTest.java
@@ -5,10 +5,15 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.vlytvyn.tasks.hello_rest.models.Contact;
 import org.vlytvyn.tasks.hello_rest.repositories.ContactRepository;
 import org.vlytvyn.tasks.hello_rest.servises.JsonWriter;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -29,16 +34,17 @@ public class ContractServiceImplTest {
     @Test
     public void writeRequestTest() throws Exception {
         String filterForNotEmptyResult = "^.*[i1].*$";
-        when(repository.findAll()).thenReturn(prepareTestData());
+        when(repository.findAll(any(Pageable.class))).thenReturn(prepareTestData());
         contractService.writeRequest(jsonWriter, filterForNotEmptyResult);
         verify(jsonWriter, times(2)).writeJson(isA(Contact.class));
     }
 
-    private List<Contact> prepareTestData() {
-        List<Contact> result = new LinkedList();
-        result.add(new Contact(1, "TestContract1"));
-        result.add(new Contact(2, "TestContract2"));
-        result.add(new Contact(3, "TestContract3"));
+    private Page<Contact> prepareTestData() {
+        List<Contact> contacts = new ArrayList<>(3);
+        contacts.add(new Contact(1, "TestContract1"));
+        contacts.add(new Contact(2, "TestContract2"));
+        contacts.add(new Contact(3, "TestContract3"));
+        Page<Contact> result = new PageImpl(contacts);
         return result;
     }
 }
